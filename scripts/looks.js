0873282c3dd3f78d26c2051181bbdf7c596e0b13
Aleksandra Klochkova <SandraSurgut@yandex.ru>
2022-02-06 17:54:31 +0500

    fix checkbox url

--- scripts/looks.js
+++ scripts/looks.js
@@ -1,5 +1,5 @@
 function getRandomLook(max) {
-  return window.location.origin + '/images/looks-' + Math.floor(Math.random() * max) + '.png';
+  return 'looks-' + Math.floor(Math.random() * max) + '.png';
 }
 
 function getRandomLooks(max) {
@@ -7,7 +7,7 @@ function getRandomLooks(max) {
   const imgs = document.querySelectorAll('.looks__photos-item');
   let srcs = [];
   imgs.forEach(function (item) {
-    srcs.push(item.src);
+    srcs.push(item.src.split('/').pop());
   });
 
   const countToChange = Math.floor(Math.random() * max) + 1;
@@ -22,18 +22,18 @@ function getRandomLooks(max) {
     while (srcs.indexOf(candidate) >= 0) {
       candidate = getRandomLook(totalCount);
     }
-
-    imgs[itemToChangeIndex].classList.add("looks__photos-item_hidden");
+    const candidateFullPath = './images/' + candidate;
+    imgs[itemToChangeIndex].classList.add('looks__photos-item_hidden');
     srcs[itemToChangeIndex] = candidate;
-    setTimeout(function() {
-      imgs[itemToChangeIndex].src = candidate;
-      imgs[itemToChangeIndex].classList.remove("looks__photos-item_hidden");
-    }, 1000)
+    setTimeout(function () {
+      imgs[itemToChangeIndex].src = candidateFullPath;
+      imgs[itemToChangeIndex].classList.remove('looks__photos-item_hidden');
+    }, 1000);
 
     changedItems.push(itemToChangeIndex);
   }
 
-  setTimeout(getRandomLooks, 1300, 3)
+  setTimeout(getRandomLooks, 1300, 3);
 }
 
 getRandomLooks(3);
